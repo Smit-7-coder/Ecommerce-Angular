2e9604b6c0fc64f99e6df8fd7238d76902eda73c
Sakariya Smit R <smit7.sakariya@gmail.com>
2026-08-12 17:37:55 +0530

    Fetch Products from database

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { DecimalPipe } from '@angular/common';
+import { ProductService } from '../../services/products.services';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, DecimalPipe],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
 
+  private productServices = inject(ProductService);
+
+  products = signal<any[]>([]);
+
+  ngOnInit(): void {
+
+    this.productServices.getProducts().subscribe((response: any) => {
+
+      console.log('API RESPONSE:', response);
+
+      if (response.success) {
+
+        this.products.set(response.data);
+
+        console.log('PRODUCTS ARRAY:', this.products());
+        console.log('TOTAL PRODUCTS:', this.products().length);
+
+      }
+
+    });
+
+  }
 }
